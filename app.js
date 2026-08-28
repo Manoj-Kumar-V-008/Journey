9239b23cdb934991f3e2405c3503613847a3a4ee
Manoj_Kumar_V <manojkumarvsnhsin@gmail.com>
2026-08-28 17:13:32 +0530

    added nav-bar to the page

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
+app.engine('ejs',ejsMate);
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{
     res.send("Server is working");
