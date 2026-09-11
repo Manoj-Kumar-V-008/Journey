65f35a2ecf0b41fdcb62bf27ab9e8b9cc740d13a
Manoj-Kumar-V-008 <manojkumarvsnhsin@gmail.com>
2026-09-11 15:27:45 +0530

    Implemented MVC design pattern (Model,Views,Controllers) for reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,30 +6,12 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn} = require("../middleware.js");
 const {validateReview , isReviewAuthor} = require("../middleware.js");
 
+const reviewController = require("../controllers/reviews.js");
+
 //Reviews Post Route
-router.post("/",isLoggedIn,validateReview, wrapAsync(async (req,res)=>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    req.flash("success","Successfully created new Review");
-    res.redirect(`/listings/${listing.id}`); 
-}));
+router.post("/",isLoggedIn,validateReview, wrapAsync(reviewController.createReview));
 
 //Review Delete Route
-router.delete("/:reviewId",isLoggedIn , isReviewAuthor ,wrapAsync(async (req,res)=>{
-    let {id,reviewId} = req.params;
-
-    await Listing.findByIdAndUpdate(id,{$pull:{review:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-
-    req.flash("success","Successfully Deleted the Review");
-    res.redirect(`/listings/${id}`);
-}));
+router.delete("/:reviewId",isLoggedIn , isReviewAuthor ,wrapAsync(reviewController.destroyReview));
 
 module.exports = router;
