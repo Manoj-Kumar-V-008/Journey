9898d75564331ca05ace8764342e6369bc8b353d
Manoj-Kumar-V-008 <manojkumarvsnhsin@gmail.com>
2026-09-11 15:20:00 +0530

    Implemented MVC design pattern (Model,Views,Controllers) for listing

---
+++ controllers/listings.js
@@ -0,0 +1,61 @@
+const Listing = require("../models/listing.js");
+
+
+module.exports.index = async (req,res)=>{
+    const allListings = await(Listing.find({}));
+    res.render("./listings/index.ejs",{allListings});
+}
+
+module.exports.renderNewForm = (req,res)=>{
+    res.render("listings/new.ejs");
+};
+
+module.exports.showListings = async (req,res)=>{
+    let {id} = req.params;
+    const listings = await Listing.findById(id)
+    .populate({
+        path:"reviews",
+        populate:{
+            path:"author"   
+        },
+    })
+    .populate("owner");
+
+    if(!listings){
+        req.flash("error","Listing doesn't Exist!");
+        return res.redirect("/listings");
+    }
+    res.render("./listings/show.ejs",{listings});
+};
+
+module.exports.createListing = async (req,res,next)=>{
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    await newListing.save();
+    req.flash("success","New Listing Created");
+    res.redirect("/listings");
+};
+
+module.exports.renderEditForm = async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    if(!listing){
+        req.flash("error","Listing doesn't Exist!");
+        return res.redirect("/listings");
+    }  
+    res.render("./listings/edit.ejs",{listing});
+};
+
+module.exports.updateListing = async (req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    req.flash("success","Successfully Edited the listing");
+    res.redirect("/listings");
+};
+
+module.exports.destroyListing = async (req,res)=>{
+    let {id} = req.params;
+    let deleteListing = await Listing.findByIdAndDelete(id);
+    req.flash("success","Deleted the Listing");
+    res.redirect("/listings");
+}
