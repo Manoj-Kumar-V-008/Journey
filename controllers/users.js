c37c72894ed0a4017e9dc2d6f4ccc27038a2d232
Manoj-Kumar-V-008 <manojkumarvsnhsin@gmail.com>
2026-09-11 15:59:48 +0530

    Implemented MVC design pattern (Model,Views,Controllers) for users

---
+++ controllers/users.js
@@ -0,0 +1,47 @@
+const User = require("../models/user.js");
+
+
+module.exports.renderSignUp = (req,res)=>{
+    res.render("users/signup.ejs");
+};
+
+module.exports.renderLogin = async(req,res)=>{
+    res.render("users/login.ejs");
+};
+
+module.exports.signup = async(req,res,next)=>{
+    try{
+        let {username,email,password} = req.body;
+        const newUser = new User({email,username});
+        const regesterdUser = await User.register(newUser,password);
+        req.login(regesterdUser,(err)=>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success",`${username} registered successfully`);
+            const redirectUrl = req.session.redirectUrl || "/listings";
+            delete req.session.redirectUrl;
+            res.redirect(redirectUrl);
+        });
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signup");
+    }
+};
+
+module.exports.login = async(req,res)=>{
+        req.flash("success",`Logged in Successfully`);
+//req.session.redirectUrl is reset by passport so we should use locals which can be accessed everywhere & passport can't change it
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
+};
+
+module.exports.logout=(req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+            next(err);
+        }
+        req.flash("success","You logged out");
+        res.redirect("/listings");
+    })
+};
--- routes/user.js
+++ routes/user.js
@@ -1,56 +1,23 @@
 const express = require("express");
 const router = express.Router();//{ mergeParams: true } helps use :id from parent(app.js) in req.params.id
-const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
+const userController = require("../controllers/users.js");
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs");
-});
+router.get("/signup",userController.renderSignUp);
 
-router.post("/signup", wrapAsync(async(req,res)=>{
-    try{
-        let {username,email,password} = req.body;
-        const newUser = new User({email,username});
-        const regesterdUser = await User.register(newUser,password);
-        req.login(regesterdUser,(err)=>{
-            if(err){
-                return next(err);
-            }
-            req.flash("success",`${username} registered successfully`);
-            res.redirect(req.session.redirectUrl);
-        });
-    }catch(e){
-        req.flash("error",e.message);
-        res.redirect("/signup");
-    }
-}));
+router.post("/signup", wrapAsync(userController.signup));
 
-router.get("/login",wrapAsync(async(req,res)=>{
-    res.render("users/login.ejs");
-}));
+router.get("/login",wrapAsync(userController.renderLogin));
 
 // passport is used as middleware for authentication during login
 router.post("/login",saveRedirectUrl,passport.authenticate('local',{failureRedirect:"/login" , failureFlash:true}),
-    wrapAsync(async(req,res)=>{
-        req.flash("success",`Logged in Successfully`);
-//req.session.redirectUrl is reset by passport so we should use locals which can be accessed everywhere & passport can't change it
-        let redirectUrl = res.locals.redirectUrl || "/listings";
-        res.redirect(redirectUrl);
-}));
+    wrapAsync(userController.login));
 
 
 //implementing logout,logout() is builtin by passport
-router.post("/logout",(req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
-            next(err);
-        }
-        req.flash("success","You logged out");
-        res.redirect("/listings");
-    })
-});
+router.post("/logout",userController.logout);
 
 module.exports=router;
